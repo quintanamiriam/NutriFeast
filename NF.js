706a91f549eeb32d37222b427efd2e145dce5191
Miriam Quintana <quintana.miriam.cc@gmail.com>
2017-08-08 13:40:52 -0600

    did GET request for edamam api

--- NF.js
+++ NF.js
@@ -5,3 +5,31 @@ $( document ).ready(function() {
 $(document).ready(function(){
       $('.parallax').parallax();
     });
+
+    //       //  $.get(url, function(data)  {
+    //       //          $.each (data, function(){
+    //       //              $(".recipesField").append( );
+    //       //              console.log(data);
+    //       //          });
+
+
+ $('.submit').on('submit', function(event) {
+   event.preventDefault()
+        var search = $("#search").val();
+      var url = "https://api.edamam.com/search?q=" + search + "&app_id=93065f52&app_key=f299940d568086c4a1e38fd8254db4bc";
+
+//       //  $.get(url, function(data)  {
+//       //          $.each (data, function(){
+//       //              $(".recipesField").append( );
+//       //              console.log(data);
+//       //          });
+$.ajax({
+  type: "GET",
+  url: url,
+  dataType: 'json',
+  success: function(data) {
+
+    console.log(data);
+  }
+})
+});
