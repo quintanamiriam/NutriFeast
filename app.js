8b5c40fd6ca1d388874f185f098334227a0e5f45
Miriam Quintana <quintana.miriam.cc@gmail.com>
2017-08-22 14:05:53 -0600

    cleaned up code, working on nutrient display';'
     git commit -m "cleaned up code, working on nutrient display"
    ~
    
    "
    quote

--- app.js
+++ app.js
@@ -1,72 +1,50 @@
-$( document ).ready(function() {
-     $(".button-collapse").sideNav();
-     $('.parallax').parallax();
+$(document).ready(function() {
+  $(".button-collapse").sideNav();
+  $('.parallax').parallax();
 });
 
-// $(document).ready(function(){
-//       $('.parallax').parallax();
-//     });
+var API = "https://api.edamam.com/search?q="
+var API_KEY_ID = "&app_id=93065f52&app_key=f299940d568086c4a1e38fd8254db4bc"
 
-    //       //  $.get(url, function(data)  {
-    //       //          $.each (data, function(){
-    //       //              $(".recipesField").append( );
-    //       //              console.log(data);
-    //       //          });
+$('.submit').on('submit', function(event) {
+  event.preventDefault()
+  var search = $("#search").val();
+  var API_URL = API + search + API_KEY_ID;
 
 
- $('.submit').on('submit', function(event) {
-   event.preventDefault()
-        var search = $("#search").val();
-      var url = "https://api.edamam.com/search?q=" + search + "&app_id=93065f52&app_key=f299940d568086c4a1e38fd8254db4bc";
+  $.ajax({
+    type: "GET",
+    url: API_URL,
+    dataType: 'json',
+    success: function(data) {
+      var recipes = []
+      for (var i = 0; i < data.q.length; i++) {
+        recipes.push(data.hits[i])
+      }
 
-//       //  $.get(url, function(data)  {
-//       //          $.each (data, function(){
-//       //              $(".recipesField").append( );
-//       //              console.log(data);
-//       //          });
-$.ajax({
-  type: "GET",
-  url: url,
-  dataType: 'json',
-  success: function(data) {
-    // console.log(data.hits);
-for (var i = 0; i < data.q.length; i++) {
-  // console.log(data.q.hits);
-  var recipeLabel = data.hits[i].recipe.label;
-  var recipeImage = data.hits[i].recipe.image;
-  var recipeTotalNutrients = data.hits[i].recipe.totalNutrients;
-  var recipeUrl = data.hits[i].recipe.url;
-
-for (var j = 0; j < data.hits[j].recipe.recipeTotalNutrients.length; j++) {
-var recipeTotalNutrientsLabel = recipeTotalNutrients[j].label
-var recipeTotalNutrientsQuantity = recipeTotalNutrients[j].quantity
-var recipeTotalNutrientsUnit = recipeTotalNutrients[j].unit
-  // console.log(recipeLabel);
-  // console.log(recipeImage);
-  // console.log(recipeIngredientLines);
-  // console.log(recipeTotalNutrients);
-  // console.log(recipeUrl);
-$(".recipesField").append(`
-  <div>
-    <div class="card col l3">
-      <div class="card-image waves-effect waves-block waves-light">
-        <img class="activator" style="width: 50%" src=${recipeImage}>
-      </div>
-      <div class="card-content">
-        <span class="card-title activator grey-text text-darken-4">${recipeLabel}<i class="material-icons right">more_vert</i></span>
-        <p><a target="blank" href=${recipeUrl}>Check out the recipe</a></p>
-      </div>
-      <div class="card-reveal">
-        <span class="card-title grey-text text-darken-4">${recipeLabel}<i class="material-icons right">close</i></span>
-        <p>${recipeTotalNutrientsList}</p>
-      </div>
-    </div>
-  </div>`)
+      for (var i = 0; i < recipes.length; i++) {
+        var recipeLabel = recipes[i].recipe.label;
+        var recipeImage = recipes[i].recipe.image;
+        var recipeUrl = recipes[i].recipe.url;
 
 
 
-}
-}
-}
-})
-})
+        $(".recipesField").append(`
+            <div class="card">
+              <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" style="width: 50%" src=${recipeImage}>
+              </div>
+              <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4">${recipeLabel}<i class="material-icons right">more_vert</i></span>
+                <p><a target="blank" href=${recipeUrl}>Check out the recipe</a></p>
+              </div>
+              <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">${recipeLabel}<i class="material-icons right">close</i></span>
+                <p>${recipeTotalNutrients}.</p>
+              </div>
+            </div>
+          </div>`)
+      }
+    }
+  })
+});
